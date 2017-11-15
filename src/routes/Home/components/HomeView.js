a9b3a8bb8fc2123262f1542630af5d2425515de5
xuewanting <541006240@qq.com>
2017-11-15 18:59:55 +0800

    updating

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -20,9 +20,14 @@ class HomeView extends React.Component {
     return (
       <div>
         <div className="home_head">
-          <h1>创新实验室</h1>
-          <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur incidunt, porro exercitationem. Quas harum fugiat, necessitatibus odio eos eligendi deleniti doloribus</h2>
-          <button className='btn'>所有实验室</button>
+          <h1>移动互联软件技术实验室</h1>
+          <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;实验室建于2014年，是在西安市“国家级软件外包服务基地”建设需要以及
+          西安文理学院专业和科学建设需要的大背景下，依托西安文理学院和中国软件国际有限
+          公司在移动互联网技术及软件开发方面的科技实力，进行严学研结合的校级重点实验室。
+          实验室现有面积90余平米，移动互联软件开发实验设备齐全，办公设施完善。实验室注重校企合作
+          与人才培养，围绕国家战略需求和地方经济产业特色优势，积极开展严学研和成果转化，打造移动
+          互联网软件开发应用型人才，从而促进行业技术进步，服务地方经济和社会发展。</h2>
+          <button className='btn'>实验室详情</button>
           <p>MORE</p>
           <a  ><i className='fa fa-angle-double-down fa-3x'></i></a>
         </div>
