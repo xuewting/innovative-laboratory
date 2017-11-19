f8c54bb95d045f0d242802f59351dc0890406e4f
xuewanting <541006240@qq.com>
2017-11-19 15:15:49 +0800

    updating

---
+++ src/routes/Pinfo/components/Basic.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Row, Col, Tooltip} from 'antd'
+
+class Basic extends Component {
+  render() {
+    return (
+      <div className='basic'>
+        
+      </div>
+    );
+  }
+}
+
+export default Basic;
--- src/routes/Pinfo/components/Pinfo.js
+++ src/routes/Pinfo/components/Pinfo.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './Pinfo.scss'
 import ResetPass from './ResetPass'
-import { Input, Row, Col, Radio, Upload,Icon,message } from 'antd'
+import head from './0824ab18972bd40790e0d4b571899e510fb30956.jpg'
+import { Input, Row, Col, Radio, Upload, Icon, message } from 'antd'
 
 const RadioGroup = Radio.Group;
 
@@ -15,9 +16,9 @@ class Pinfo extends Component {
       name: 'haha',
       phone: 11111111111,
       mail: '541006240@qq.com',
-      disabled2:false,
-      imageUrl:'',
-      resetPass:0,
+      disabled2: false,
+      imageUrl: '',
+      resetPass: 0,
     }
   }
 
@@ -30,14 +31,14 @@ class Pinfo extends Component {
 
           if (len > 6 || len == 0) {
             this.refs.name.style.display = 'inline'
-            this.setState({disabled2:true})
-            this.refs.submit.style.background='#f74f4f'
-            this.refs.submit.style.borderColor='#c02f2f'
+            this.setState({ disabled2: true })
+            this.refs.submit.style.background = '#f74f4f'
+            this.refs.submit.style.borderColor = '#c02f2f'
           } else {
             this.refs.name.style.display = 'none'
-            this.setState({disabled2:false})
-            this.refs.submit.style.background='#13918a'
-            this.refs.submit.style.borderColor='#106b65'
+            this.setState({ disabled2: false })
+            this.refs.submit.style.background = '#13918a'
+            this.refs.submit.style.borderColor = '#106b65'
           }
           break;
         }
@@ -47,14 +48,14 @@ class Pinfo extends Component {
 
           if (len != 11) {
             this.refs.phone.style.display = 'inline'
-            this.setState({disabled2:true})
-            this.refs.submit.style.background='#f74f4f'
-            this.refs.submit.style.borderColor='#c02f2f'
+            this.setState({ disabled2: true })
+            this.refs.submit.style.background = '#f74f4f'
+            this.refs.submit.style.borderColor = '#c02f2f'
           } else {
             this.refs.phone.style.display = 'none'
-            this.setState({disabled2:false})
-            this.refs.submit.style.background='#13918a'
-            this.refs.submit.style.borderColor='#106b65'
+            this.setState({ disabled2: false })
+            this.refs.submit.style.background = '#13918a'
+            this.refs.submit.style.borderColor = '#106b65'
           }
           break;
         }
@@ -68,14 +69,14 @@ class Pinfo extends Component {
           this.setState({ sid: value });
           if (len != 10) {
             this.refs.sid.style.display = 'inline'
-            this.setState({disabled2:true})
-            this.refs.submit.style.background='#f74f4f'
-            this.refs.submit.style.borderColor='#c02f2f'
+            this.setState({ disabled2: true })
+            this.refs.submit.style.background = '#f74f4f'
+            this.refs.submit.style.borderColor = '#c02f2f'
           } else {
             this.refs.sid.style.display = 'none'
-            this.setState({disabled2:false})
-            this.refs.submit.style.background='#13918a'
-            this.refs.submit.style.borderColor='#106b65'
+            this.setState({ disabled2: false })
+            this.refs.submit.style.background = '#13918a'
+            this.refs.submit.style.borderColor = '#106b65'
           }
           break;
         }
@@ -83,11 +84,11 @@ class Pinfo extends Component {
   }
 
   changeState(type) {
-    if(type==1){
-    this.setState({ disabled: !this.state.disabled })
-  }else{
-    this.setState({resetPass:1})
-  }
+    if (type == 1) {
+      this.setState({ disabled: !this.state.disabled })
+    } else {
+      this.setState({ resetPass: 1 })
+    }
   }
 
   onChange(e) {
@@ -98,93 +99,36 @@ class Pinfo extends Component {
       color: '#f74f4f',
       display: 'none',
     }
-    const { disabled, disabled2, name, mail, sid, phone, imageUrl,resetPass } = this.state
+    const { disabled, disabled2, name, mail, sid, phone, imageUrl, resetPass } = this.state
 
     return (
       <div className='Pinfo'>
-      {resetPass==0?
-      <div>
-        <h1 className="pintitle">个人信息</h1>
-
-        <Row className="inbox">
-          <Col span={3}><p>姓名：</p></Col>
-          <Col span={10}>
-            <Input
-              className={disabled==false?'in2':'in'}            
-              disabled={disabled}
-              onChange={(e) => this.changeValue(e.target.value, 1)}
-              placeholder={name}
-              value={name}>
-            </Input></Col>
-          <Col span={2}><i className='fa fa-times fa-1x' style={style} ref='name'></i></Col>
-        </Row>
-
-        <Row className="inbox">
-          <Col span={3}><p>邮箱：</p></Col>
-          <Col span={10}>
-            <Input
-              className={disabled==false?'in2':'in'}              
-              disabled={disabled}
-              onChange={(e) => this.changeValue(e.target.value, 3)}
-              placeholder={mail}
-              value={mail}>
-            </Input></Col>
-          <Col span={2}><i className='fa fa-times fa-1x' style={style} ref='mail'></i></Col>
-        </Row>
-
-        <Row className="inbox">
-          <Col span={3}><p>手机号：</p></Col>
-          <Col span={10}>
-            <Input
-              className={disabled==false?'in2':'in'}
-              disabled={disabled}
-              onChange={(e) => this.changeValue(e.target.value, 2)}
-              placeholder={phone}
-              value={phone}>
-            </Input></Col>
-          <Col span={2}><i className='fa fa-times fa-1x' style={style} ref='phone'></i></Col>
-        </Row>
-
-        <Row className="inbox">
-          <Col span={3}><p>性别：</p></Col>
-          <Col span={10}>
-            <RadioGroup onChange={this.onChange.bind(this)} value={this.state.sex} disabled={disabled}>
-              <Radio value={1}>男</Radio>
-              <Radio value={2}>女</Radio>
-            </RadioGroup>
+        <Row style={{ width: '100%' }} className='con'>
+          <Col span={4} className='lef'>
+            <div className="sidebar">
+              <div className='touxiang'>
+                <img src={head} alt="" className='img' />
+              </div>
+              <div className="select">
+                <span><i className='fa fa-id-card-o'></i> </span>
+                <span> 基本信息</span><span className='i'><i className=' fa fa-angle-right'></i></span>
+              </div>
+              <div className="select">
+                <span><i className='fa fa-suitcase'></i> </span>
+                <span> 所有项目</span><span className='i'><i className=' fa fa-angle-right'></i></span>
+              </div>
+              <div className="select">
+                <span><i className='fa fa-home'></i> </span>
+                <span> 实验室</span><span className='i'><i className=' fa fa-angle-right'></i></span>
+              </div>
+              <div className="select" style={{ border: 'none' }}>
+                <span><i className='fa fa-envelope-o'></i> </span>
+                <span> 消息</span><span className='i'><i className=' fa fa-angle-right'></i></span>
+              </div>
+            </div>
           </Col>
+          <Col span={18} offset={1}>111</Col>
         </Row>
-
-        <Row className="inbox">
-          <Col span={3}><p>身份：</p></Col>
-          <Col span={10}><p>学生</p></Col>
-        </Row>
-
-        <Row className="inbox">
-          <Col span={3}><p>学号：</p></Col>
-          <Col span={10}>
-            <Input
-              className={disabled==false?'in2':'in'}             
-              disabled={disabled}
-              onChange={(e) => this.changeValue(e.target.value, 4)}
-              placeholder={sid}
-              value={sid}>
-            </Input></Col>
-          <Col span={2}><i className='fa fa-times fa-1x' style={style} ref='sid'></i></Col>
-        </Row>
-        <Row style={{width:'30%',marginLeft:'auto',marginRight:'auto'}}>
-        <Col span={12}>
-        <button onClick={this.changeState.bind(this,1)} disabled={disabled2} ref='submit'>
-          {disabled == true ? '修改信息' : '保存'}
-        </button>
-        </Col>
-        <Col span={12}>
-        <button  onClick={this.changeState.bind(this,2)}>
-          修改密码
-        </button>
-        </Col>
-        </Row>
-        </div>:<ResetPass></ResetPass>}
       </div>
     );
   }
